acda9e004c100cd1457a340edff6ae3faca0612a
Moose Davis <moosedavis2011@gmail.com>
2020-09-17 22:39:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(student_name, student_grade)
+    @roster[student_grade] = student_name
+    
+  end 
+
+end 
